817a2dc011ef7a9741d0292024da5fdcff24622e
Daniel Silva <daniel.mascr@gmail.com>
2019-03-05 19:07:37 -0300

    Add classe abstrata Pessoa

---
+++ abstract/src/entities/Pessoa.java
@@ -0,0 +1,26 @@
+package entities;
+
+public abstract class Pessoa {
+	
+	private String nome;
+	private Double rendaAnual;
+	
+	public Pessoa() {
+	}
+	
+	public Pessoa(String nome, Double rendaAnual) {
+		super();
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public Double getRendaAnual() {
+		return rendaAnual;
+	}
+	
+	public abstract Double calculaImposto();	
+}
