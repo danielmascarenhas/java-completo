30332f52d7c4e0e023ffce69f2b4d03f60722e0c
Daniel Silva <daniel.mascr@gmail.com>
2019-03-05 20:02:28 -0300

    Add classe PessoaJuridica

---
+++ abstract/src/entities/PessoaJuridica.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class PessoaJuridica extends Pessoa {
+
+	private Integer numeroDeFuncionarios;
+	
+	public PessoaJuridica() {
+		super();
+	}
+	
+	public PessoaJuridica(String nome, Double rendaAnual, Integer numeroDeFuncionarios) {
+		super(nome, rendaAnual);
+		this.numeroDeFuncionarios = numeroDeFuncionarios;
+	}
+
+	@Override
+	public Double calculaImposto() {
+		return numeroDeFuncionarios < 10 ? getRendaAnual() * 0.16 : getRendaAnual() * 0.14;
+	}
+
+}
